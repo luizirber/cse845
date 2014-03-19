6c3d261ba9937c9f458d5a84dad9a913b3a30b9c
Thomas LaBar <tlabar7@gmail.com>
2014-03-19 14:17:53 -0400

    Changes to analyze modularity

--- chow_replication/replication_data_analysis_files/run_analysis.py
+++ chow_replication/replication_data_analysis_files/run_analysis.py
@@ -1,5 +1,6 @@
 import analysis_code as ac
 
-ac.get_final_ecotype_num_data(["01","1","10","100","1000","10000","100000"],"phenotype")
-ac.get_final_ecotype_num_data(["01","1","10","100","1000","10000","100000"],"genotype")
-ac.get_final_task_num_ecotypes(["01","1","10","100","1000","10000","100000"])
+#ac.get_final_ecotype_num_data(["01","1","10","100","1000","10000","100000"],"phenotype")
+#ac.get_final_ecotype_num_data(["01","1","10","100","1000","10000","100000"],"genotype")
+#ac.get_final_task_num_ecotypes(["01","1","10","100","1000","10000","100000"])
+ac.get_site_proportion_data(["1","100"])
